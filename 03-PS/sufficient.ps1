604970e3c288aa1e2a13af03c25f608ffc6dba75
Ananas97 <ana.rei.014@gmail.com>
2020-03-18 17:27:41 +0100

    add solution for task 2

--- 03-PS/sufficient.ps1
+++ 03-PS/sufficient.ps1
@@ -7,4 +7,19 @@ return $a * $b
 #invocation
 Multiply 2 4
 Multiply -a 5 -b 2
+#task 2
+#implementation
+function Increment 
+{
+Param
+([Parameter(Mandatory=$true,Position=0)]
+[int]$number,
+[Parameter(Mandatory=$false, Position=1)]
+[int]$incrementValue=1)
 
+return $number + $incrementValue;
+}
+#invocation
+Increment 2 5 
+Increment 3    
+Increment 
