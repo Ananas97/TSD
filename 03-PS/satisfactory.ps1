8eda89fe8927ab8c4d7c83ef895fb9657d2d4ab4
unknown <ana.rei.014@gmail.com>
2020-03-20 12:41:51 +0100

    partially resolved task 3

--- 03-PS/satisfactory.ps1
+++ 03-PS/satisfactory.ps1
@@ -5,3 +5,8 @@ $chfeed.rss.channel.item | Select-Object title
 
 # task 1
 Get-Process | Select-Object CPU, ProcessName | Where-Object { $_.CPU -gt 10 }> C:\Users\Ananas\Desktop\OUTPUT.txt
+
+# task3
+$file = Invoke-WebRequest -Uri https://sleepy-wilson-d2b50e.netlify.com/powershell/MSDN.txt
+$file.Content | findstr "PowerShell"
+#number of line is missing
