e2406ec0dee10df23fd9acc86e0dc3731f68d03c
unknown <ana.rei.014@gmail.com>
2020-03-20 11:42:47 +0100

    solution for task 1

--- 03-PS/satisfactory.ps1
+++ 03-PS/satisfactory.ps1
@@ -1,3 +1,7 @@
 # task 5
 $chfeed = [xml](Invoke-WebRequest "https://sleepy-wilson-d2b50e.netlify.com/powershell/PowerShell.rss")
 $chfeed.rss.channel.item | Select-Object title
+
+
+# task 1
+Get-Process | Select-Object CPU, ProcessName | Where-Object { $_.CPU -gt 10 }> C:\Users\Ananas\Desktop\OUTPUT.txt
