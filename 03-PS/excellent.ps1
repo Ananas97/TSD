90ea4d7f54ad905d291d3e9bbf04fa3f80c79590
unknown <ana.rei.014@gmail.com>
2020-03-21 15:57:30 +0100

    show seconds only

--- 03-PS/excellent.ps1
+++ 03-PS/excellent.ps1
@@ -2,9 +2,9 @@
 $array = foreach($i in 1..10000) { 
   Get-Random
 }
-Measure-Command {$array | Sort-Object}
+Measure-Command {$array | Sort-Object} | Select-Object -Property TotalSeconds
 $DOTnet = New-Object System.Collections.ArrayList
  foreach($i in $array.Length) { 
   $DOTnet.Add([int]$array[$i])
 }
-Measure-Command {$DOTnet.Sort()}
+Measure-Command {$DOTnet.Sort()} | Select-Object -Property TotalSeconds
