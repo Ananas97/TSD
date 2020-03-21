fb885e1a19251d66fc57842d5d21c6970ac04300
unknown <ana.rei.014@gmail.com>
2020-03-21 15:47:51 +0100

    solution using ArrayList

--- 03-PS/excellent.ps1
+++ 03-PS/excellent.ps1
@@ -1,1 +1,10 @@
-# task 
+# task  1
+$array = foreach($i in 1..10000) { 
+  Get-Random
+}
+Measure-Command {$array | Sort-Object}
+$DOTnet = New-Object System.Collections.ArrayList
+ foreach($i in $array.Length) { 
+  $DOTnet.Add([int]$array[$i])
+}
+Measure-Command {$DOTnet.Sort()}
