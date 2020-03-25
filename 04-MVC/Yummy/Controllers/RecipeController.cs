0a541835e33198af2fe943437e276cd23cf4da30
unknown <ana.rei.014@gmail.com>
2020-03-25 18:44:33 +0100

    create a new controller

---
+++ 04-MVC/Yummy/Controllers/RecipeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Yummy.Controllers
+{
+    public class RecipeController : Controller
+    {
+        //
+        // GET: /Recipe/list
+        public IActionResult Index()
+        //public string Index()
+        {
+            return View();
+            //return "This is the Index action method...";
+        }
+
+        // 
+        // GET: /Recipe/add
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
