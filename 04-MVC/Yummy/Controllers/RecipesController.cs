cfbd768aa6f1b8edbedb83b47f30a8a480dbd290
unknown <ana.rei.014@gmail.com>
2020-03-26 19:57:51 +0100

    add search by Name attribute

--- 04-MVC/Yummy/Controllers/RecipesController.cs
+++ 04-MVC/Yummy/Controllers/RecipesController.cs
@@ -20,9 +20,17 @@ namespace Yummy.Controllers
         }
 
         // GET: Recipes
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string searchString)
         {
-            return View(await _context.Recipe.ToListAsync());
+            var recipes = from r in _context.Recipe
+                         select r;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                recipes = recipes.Where(s => s.Name.Contains(searchString));
+            }
+
+            return View(await recipes.ToListAsync());
         }
 
         // GET: Recipes/Details/5
