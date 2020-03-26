618ce2496a67efc11d8424d610d2f521a528c5bf
unknown <ana.rei.014@gmail.com>
2020-03-26 16:52:33 +0100

    show culumn names differently with spaces as it was written in the task extract for sufficient

--- 04-MVC/Yummy/Models/Recipe.cs
+++ 04-MVC/Yummy/Models/Recipe.cs
@@ -13,9 +13,11 @@ namespace Yummy.Models
         public string Name { get; set; }
         public int Time { get; set; }
         public string Difficulty { get; set; }
+        [Display(Name = "Number of likes")]
         public decimal NumberOfLikes { get; set; }
         public string Ingredients { get; set; }
         public string Process { get; set; }
+        [Display(Name = "Tips and Tricks")]
         public string TipsAndTricks { get; set; }
     }
 }
