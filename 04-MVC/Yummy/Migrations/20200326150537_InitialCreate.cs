3ac3ccf1c71ced8c499c667900c24fc9893dab29
unknown <ana.rei.014@gmail.com>
2020-03-26 16:16:56 +0100

    sufficient completed

---
+++ 04-MVC/Yummy/Migrations/20200326150537_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Yummy.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Recipe",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Time = table.Column<int>(nullable: false),
+                    Difficulty = table.Column<string>(nullable: true),
+                    NumberOfLikes = table.Column<decimal>(nullable: false),
+                    Ingredients = table.Column<string>(nullable: true),
+                    Process = table.Column<string>(nullable: true),
+                    TipsAndTricks = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Recipe", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Recipe");
+        }
+    }
+}
