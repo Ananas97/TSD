cf3f58af6b15ec4feaa5f9395034b85a78578b05
unknown <ana.rei.014@gmail.com>
2020-03-26 20:25:04 +0100

    satistactory completed

--- 04-MVC/Yummy/Models/Recipe.cs
+++ 04-MVC/Yummy/Models/Recipe.cs
@@ -10,8 +10,11 @@ namespace Yummy.Models
     public class Recipe
     {
         public int ID { get; set; }
+        [Required, StringLength(50, MinimumLength = 3)]
         public string Name { get; set; }
+        [Required]
         public int Time { get; set; }
+        [Required, Range(1, 5)]
         public string Difficulty { get; set; }
         [Display(Name = "Number of likes")]
         public decimal NumberOfLikes { get; set; }
