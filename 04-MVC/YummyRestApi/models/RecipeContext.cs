9983a3a9446953dfa224cdd09f6cda3e436ec4cd
Ananas <ana.rei.014@gmail.com>
2020-03-28 22:02:00 +0100

    add project for yummyrestapi

---
+++ 04-MVC/YummyRestApi/models/RecipeContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace YummyRestApi.Models
+{
+    public class RecipeContext : DbContext
+    {
+        public RecipeContext(DbContextOptions<RecipeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RecipeItem> RecipeItems { get; set; }
+    }
+}
---
+++ 04-MVC/YummyRestApi/models/RecipeItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YummyRestApi.Models
+{
+    public class RecipeItem
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Time { get; set; }
+        public string Difficulty { get; set; }
+   
+        public decimal NumberOfLikes { get; set; }
+        public string Ingredients { get; set; }
+        public string Process { get; set; }
+        public string TipsAndTricks { get; set; }
+    }
+}
