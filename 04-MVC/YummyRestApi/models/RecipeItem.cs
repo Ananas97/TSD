1e6f50ee3914ef3abe8db33bf7a48cce6319054f
Ananas <ana.rei.014@gmail.com>
2020-03-29 16:53:34 +0200

    change type from decimal to int, so it could be converted from json format when using postman

--- 04-MVC/YummyRestApi/models/RecipeItem.cs
+++ 04-MVC/YummyRestApi/models/RecipeItem.cs
@@ -12,7 +12,7 @@ namespace YummyRestApi.Models
         public int Time { get; set; }
         public string Difficulty { get; set; }
    
-        public decimal NumberOfLikes { get; set; }
+        public int NumberOfLikes { get; set; }
         public string Ingredients { get; set; }
         public string Process { get; set; }
         public string TipsAndTricks { get; set; }
