a68601398ecce48165c0253ff1cca1121fc27881
Ananas97 <ana.rei.014@gmail.com>
2020-04-15 19:58:12 +0200

    Update excellent.rb

--- 07-RUBY/excellent.rb
+++ 07-RUBY/excellent.rb
@@ -1,4 +1,5 @@
 
+
 class Clock
 
   attr_reader :hours, :minutes, :seconds
@@ -15,22 +16,20 @@ class Clock
 
   def +(number)
     altered_hours = (@clock_hours + ((@clock_minutes + number) / 60)) % 24
-    altered_minutes = (@clock_minutes + number + (@clock_seconds + number) / 60) % 60
-    altered_seconds = (@clock_seconds + number) % 60
-    Clock.new(altered_hours, altered_minutes, altered_seconds)
+    altered_minutes = (@clock_minutes + number) % 60
+    Clock.new(altered_hours, altered_minutes, @clock_seconds)
   end
 
   def -(number)
     altered_hours = (@clock_hours + ((@clock_minutes + number) / 60)) % 24
-    altered_minutes = (@clock_minutes - number -  (@clock_seconds + number) / 60) % 60
-    altered_seconds = (@clock_seconds - number) % 60
-    Clock.new(altered_hours, altered_minutes, altered_seconds)
+    altered_minutes = (@clock_minutes - number) % 60
+    Clock.new(altered_hours, altered_minutes, @clock_seconds)
   end
 
   def ==(different_clock)
-    if !(@clock_hours == different_clock.hours and
+    if (!(@clock_hours == different_clock.hours and
         @clock_minutes == different_clock.minutes and
-        @clock_seconds == different_clock.seconds)
+        @clock_seconds == different_clock.seconds))
       return false
     else return true
     end
@@ -41,4 +40,5 @@ clock = Clock.new(10, 0, 0)
 clock.print  # The current time is 10:00:00
 clock += 30
 clock.print  # The current time is 10:30:00
-puts(clock == Clock.new(10, 30, 0)) # true
+clock == Clock.new(10, 30, 0) # true
+
