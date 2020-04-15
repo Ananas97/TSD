00ebf16f7d39905fad0a84ff72c800778914e69c
Ananas97 <ana.rei.014@gmail.com>
2020-04-15 19:03:47 +0200

    task 2 solved

--- 07-RUBY/satisfactory.rb
+++ 07-RUBY/satisfactory.rb
@@ -5,3 +5,13 @@ end
 
 pangram?("abba") # false
 pangram?("The quick brown fox jumps over the lazy dog.") # true
+
+#task 2
+def decimal(binary_string)
+  if(!(binary_string.chars.all? {|x| x =~ /[01]/}))
+  raise 'ArgumentError: this is not a binary number'
+  end
+
+  puts(binary_string.to_i(2))
+end
+decimal("101") # 5
