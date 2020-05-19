f62c6586ad7fb372c6a24fd3ab43711e5ed04433
Ananas97 <ana.rei.014@gmail.com>
2020-05-19 19:19:12 +0200

    complete the last step so every test passes

--- NUnit/PrimeService/PrimeService.cs
+++ NUnit/PrimeService/PrimeService.cs
@@ -6,7 +6,7 @@ namespace Prime.Services
     {
         public bool IsPrime(int candidate)
         {
-            if (candidate == 1)
+            if (candidate < 2)
             {
                 return false;
             }
